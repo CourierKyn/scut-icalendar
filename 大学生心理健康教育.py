302c6c78b7bccb194f28cedbb7b464baac3e4029
Courier Kyn <franklin_a_h_p@qq.com>
2019-03-15 22:33:54 +0800

    加入个人课程信息

---
+++ 大学生心理健康教育.py
@@ -0,0 +1,8 @@
+[{'class_period': (5, 7),
+  'location': '大学城校区 A1102',
+  'name': '大学生心理健康教育',
+  'professor': '刘妍君',
+  'week_day': 3,
+  'week_period': (4, 10),
+  'week_period_text': '4-10周'},
+  datetime.date(2019, 2, 25)]
---
+++ 跨文化交际.py
@@ -0,0 +1,8 @@
+[{'class_period': (5, 7),
+  'location': '大学城校区 A5603',
+  'name': '跨文化交际',
+  'professor': '外教10',
+  'week_day': 2,
+  'week_period': (4, 15),
+  'week_period_text': '4-15周'},
+  datetime.date(2019, 2, 25)]
